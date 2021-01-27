4843e1a8f7e0b8efc5b9c57da9d519885352beb2
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-01-27 15:02:48 -0500

    two number sum again

--- easy/two-number-sum.js
+++ easy/two-number-sum.js
@@ -1,19 +1,35 @@
+// function twoNumberSum(array, targetSum) {
+//     let hash = {};
+
+//     for (let i = 0; i < array.length; i++) {
+//         let current = array[i];
+//         let different = targetSum - current;
+
+//         if (hash.hasOwnProperty(different)) {
+//             return [current, different]. sort((a, b) => a -b);
+//         } else {
+//             hash[current] = true;
+//         }
+//     }
+
+//     return [];
+// }
+
+// twoNumberSum([4, 6], 10);
+// twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
+
 function twoNumberSum(array, targetSum) {
-    let hash = {};
-
     for (let i = 0; i < array.length; i++) {
-        let current = array[i];
-        let different = targetSum - current;
+        const firstNum = array[i];
 
-        if (hash.hasOwnProperty(different)) {
-            return [current, different]. sort((a, b) => a -b);
-        } else {
-            hash[current] = true;
-        }
+    for (let j = i + 1; j < array.length; j++) {
+        const secondNum = array[j];
+        if (firstNum + secondNum === targetSum) {
+            return[firstNum, secondNum]
+         }
+      }
     }
-
     return [];
 }
 
-twoNumberSum([4, 6], 10);
-twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
+console.log(twoNumberSum);

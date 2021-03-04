1058d9b1f2ac8a00b27d7f2b9b6ea9b920e53eea
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-03-04 05:51:08 -0500

    binary search

---
+++ easy/binary-search.js
@@ -0,0 +1,18 @@
+function binarySearch(array, target) {
+	return bsHelper(array, target, 0, array.length - 1);
+}
+
+function bsHelper(array, target, left, right) {
+	if (left > right) return -1;
+	const middle = Math.floor((left + right) / 2);
+	const potentialMatch = array[middle];
+	if (target === potentialMatch) {
+		return middle;
+	} else if (target < potentialMatch) {
+		return bsHelper(array, target, left, middle - 1);
+	} else {
+		return bsHelper(array, target, middle + 1, right);
+	}
+}
+
+console.log(binarySearch());

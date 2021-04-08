39e17ac97cab1d228933eb8d6c1bb9e371416b5d
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-04-08 17:14:37 -0400

    two number sum

--- easy/two-number-sum.js
+++ easy/two-number-sum.js
@@ -19,17 +19,20 @@
 // twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
 
 function twoNumberSum(array, targetSum) {
-    for (let i = 0; i < array.length; i++) {
-        const firstNum = array[i];
+	for (let i = 0; i < array.length; i++) {
+		const firstNum = array[i];
 
-    for (let j = i + 1; j < array.length; j++) {
-        const secondNum = array[j];
-        if (firstNum + secondNum === targetSum) {
-            return[firstNum, secondNum]
-         }
-      }
-    }
-    return [];
+		for (let j = i + 1; j < array.length; j++) {
+			const secondNum = array[j];
+			if (firstNum + secondNum === targetSum) {
+				return [ firstNum, secondNum ];
+			}
+		}
+	}
+	return [];
 }
 
-console.log(twoNumberSum);
+array = [ 3, 5, 4, 8, 11, 1, -1, 6 ];
+targetSum = 10;
+
+twoNumberSum(array, targetSum);

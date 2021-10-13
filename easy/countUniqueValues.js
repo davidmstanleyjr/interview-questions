8194d3fb2e3c2ccf307afe5c15e0f7475b9fa730
davidmstanleyjr <davidmstanleyjr@gmail.com>
2021-10-12 20:34:38 -0400

    update

--- easy/countUniqueValues.js
+++ easy/countUniqueValues.js
@@ -1,17 +1,6 @@
-// implement a function called countUniqueValues, which accepts an assorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
-
-// function countUniqueValues(arr){
-//     if(arr.length === 0) return 0;
-//     var i = 0;
-//     for(var j = 1; j < arr.length; j++){
-//         if(arr[i] !== arr[j]){
-//             i++;
-//             arr[i] = arr[j]
-//         }
-//     }
-//     return i + 1;
-// }
-// countUniqueValues([1,2,2,5,7,7,99])
+// implement a function called countUniqueValues, which accepts an assorted array,
+// and counts the unique values in the array.
+// There can be negative numbers in the array, but it will always be sorted.
 
 function countUniqueValues(arr) {
 	if (arr.length === 0) return 0;

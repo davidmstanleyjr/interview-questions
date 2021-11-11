9bd73214b3b5d6c90a0fc046840ee030c340b883
davidmstanleyjr <davidmstanleyjr@gmail.com>
2021-11-10 20:02:41 -0500

    finished dog feeder logic

--- easy/dogFeeder.js
+++ easy/dogFeeder.js
@@ -2,6 +2,9 @@
 //number of pounds of raw food to feed in a day.
 
 //feeding requirements
+
+//adult dogs
+
 //adult dogs at least 1 year
 //up to 5 pounds - 5% of their body weight
 // 6-10 pounds - 4% of their body weight
@@ -12,3 +15,24 @@
 // 2 - 4 months 10% of their body weight
 // 4 - 7 months 5% of their body weight
 // 7 - 12 months 4% of their body weight
+
+// name the function dogFeeder
+
+function dogFeeder(weight, age) {
+	let rawFood = 0;
+	if (age >= 1) {
+		//adult dog
+		if (weight <= 5) {
+			rawFood = weight * 0.05;
+			return rawFood;
+		} else if (weight > 5 && weight <= 10) {
+			rawFood = weight * 0.04;
+			return rawFood;
+		}
+	} else {
+		//puppy
+		console.log("puppy");
+	}
+}
+
+console.log(dogFeeder(7, 2));

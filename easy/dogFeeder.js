44e44cbfc842b86a9420c9bf614ded6ea26beea8
davidmstanleyjr <davidmstanleyjr@gmail.com>
2021-11-10 19:18:38 -0500

    dog feeder pseudocode

---
+++ easy/dogFeeder.js
@@ -0,0 +1,14 @@
+//Takes weight in pounds and age in years and returns the
+//number of pounds of raw food to feed in a day.
+
+//feeding requirements
+//adult dogs at least 1 year
+//up to 5 pounds - 5% of their body weight
+// 6-10 pounds - 4% of their body weight
+// 11 - 15 pounds - 3% of their body weight
+// > 15 pounds - 2% of their body weight
+
+// puppies less than 1 year
+// 2 - 4 months 10% of their body weight
+// 4 - 7 months 5% of their body weight
+// 7 - 12 months 4% of their body weight

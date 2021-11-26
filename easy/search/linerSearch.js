b7f392bdb916ffa5b43a78fa2e1384759471264c
davidmstanleyjr <davidmstanleyjr@gmail.com>
2021-11-26 18:15:29 -0500

    psuedocode

--- easy/search/linerSearch.js
+++ easy/search/linerSearch.js
@@ -3,6 +3,13 @@
 // the value exists. If the value does not exist in the
 // array, return -1.
 
+//Pseudocode
+
+// This function accepts an array and a value
+// Loop through the array and check if the current array element is equal to the value.
+// If it is, return the index at which the element is found.
+// If the value is never found, return -1.
+
 function linearSearch(arr, val) {
 	for (var i = 0; i < arr.length; i++) {
 		if (arr[i] === val) return i;

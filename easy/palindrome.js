9975a6194b645a4364fd7448f9452a3854863e01
davidmstanleyjr <davidmstanleyjr@gmail.com>
2021-12-02 22:21:40 -0500

    palindrome

--- easy/palindrome.js
+++ easy/palindrome.js
@@ -1,11 +1,6 @@
-// function isPalindrome(string, i = 0) {
-// 	const j = string.length - 1 - i;
-// 	return i >= j ? true : string[i] === string[j] && isPalindrome(string, i + 1);
-// }
-
-// isPalindrome('abcdcba');
-
-
 function isPalindrome(string, i = 0) {
 	const j = string.length - 1 - i;
+	return i >= j ? true : string[i] === string[j] && isPalindrome(string, i + 1);
 }
+
+console.log(isPalindrome("abcdcba"));

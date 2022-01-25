4ca4a2881ad0edd206937f24bb49ac4556d6b9a2
davidmstanleyjr <davidmstanleyjr@gmail.com>
2022-01-24 19:52:28 -0500

    mergeSort.js

--- sort/mergeSort.js
+++ sort/mergeSort.js
@@ -3,6 +3,22 @@
 // Works by decomposing an array into smaller arrays of 0 or 1 elements, then building up a newly sorted array.
 // Divide and conquer basically.
 
+//In order to implement merge sort, it's useful to first implement
+// a function responsible for merging two sorted arrays.
+//Given two arrays which are sorted, and consists of all of
+// the elements in the two input arrays.
+//This function should run n 0(n + m) time and O(n + m) space and
+//should ot modify the parameters passed to it.
+
+// Pseudocode
+//Create an empty array, take a look at the smallest values in each input array.
+// While there are still values we havent looked at, if the value in the first array is smaller
+//then the value in the second array, push the value in the first array
+//into our results and move on to the next value in the first array.
+// If the value in the first array is larger than that of of the second array,
+//push the value in the second array into the results and move on to the next value in the second array.
+// Once you exhaust one array, push in all remaining values from the other array.
+
 function merge(arr1, arr2) {
 	let results = [];
 	let i = 0;

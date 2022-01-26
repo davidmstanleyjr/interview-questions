3ec69f1aff4fc60cd675aa855403804db8398f61
davidmstanleyjr <davidmstanleyjr@gmail.com>
2022-01-25 19:45:41 -0500

    recursiveMergeSort.js pseudocode

--- sort/recursiveMergeSort.js
+++ sort/recursiveMergeSort.js
@@ -21,7 +21,11 @@ function merge(arr1, arr2) {
 	}
 	return results;
 }
-
+//Pseudocode
+// Break up the array into halves until you have arrays that are empty or have one element.
+//Once you have smaller sorted arrays, merge those arrays with other sorted
+// arrays until you are back at the full length of the array.
+// Once the array has been merged back together, return the merged and sorted array.
 function mergeSort(arr) {
 	if (arr.length <= 1) return arr;
 	let mid = Math.floor(arr.length / 2);

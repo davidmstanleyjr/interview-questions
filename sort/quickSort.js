8264772a91692d42a8d446e793d389d4c2316a23
davidmstanleyjr <davidmstanleyjr@gmail.com>
2022-03-31 15:37:31 -0400

    quickSort.js

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -1,3 +1,19 @@
+// Like Merge Sort, QuickSort is a Divide and Conquer algorithm.
+// It picks an element as pivot and partitions the given array around the picked pivot.
+//There are many different versions of quickSort that pick pivot in different ways.
+
+// Always pick first element as pivot.
+// Always pick last element as pivot (implemented below)
+// Pick a random element as pivot.
+// Pick median as pivot.
+// The key process in quickSort is partition(). Target of partitions is, given an array and an element x of array as pivot, put x at its correct position in sorted array and put all smaller elements (smaller than x) before x, and put all greater elements (greater than x) after x. All this should be done in linear time.
+
+//it's useful to implement a function that's responsible for arranging elements in an array on either side of a pivot.
+//Given an array, this helper function should designate an element as the pivot
+//It should rearrange elements in the array so that all values less than the pivot are moved to the left of the pivot, and all values greater than the pivot are moved to the right of the pivot.
+// the order of elements on either side of the pivot doesn't matter!
+//The helper should do this in place, it shouldn't create a new array.
+
 function pivot(arr, start = 0, end = arr.length - 1) {
 	const swap = (arr, idx1, idx2) => {
 		[arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
